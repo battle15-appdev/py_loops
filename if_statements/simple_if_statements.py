80b4bfec32ef4228c1badc61f3e6c2a8c78fc155
battle15 <109545506+battle15@users.noreply.github.com>
2024-04-18 11:55:44 -0500

    Delete if_statements directory

--- if_statements/simple_if_statements.py
+++
@@ -1,8 +0,0 @@
-
-# Check If the person can vote based on age
-age = 19 
-if age >= 18:
-	print(" You are old enough to vote!")
-	# Line will run in this loop as long as the condtions are met
-	print(" Have you registered to vote yet?")
-
